df06c001b1bcf3efd7959c83b0c4b81a9f508cb2
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-10-11 16:22:06 -0300

    Create registration template

---
+++ src/main/java/com/challengeme/provider/controller/ProviderController.java
@@ -0,0 +1,17 @@
+package com.challengeme.provider.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping()
+public class ProviderController {
+
+    @GetMapping("/cadastro")
+    public ModelAndView showRegistrationForm() {
+        ModelAndView mv = new ModelAndView("registration");
+        return mv;
+    }
+}
