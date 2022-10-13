0a84677bd304c0771390560f04bae5580c117f5c
amandacbarreto <amanda.c.s.barreto@gmail.com>
2022-10-13 16:00:47 -0300

    Implements post method to user registration

--- src/main/java/com/challengeme/provider/controller/ProviderController.java
+++ src/main/java/com/challengeme/provider/controller/ProviderController.java
@@ -1,13 +1,26 @@
 package com.challengeme.provider.controller;
 
+import com.challengeme.provider.dto.UserDTO;
+import com.challengeme.provider.service.ProviderService;
+import com.challengeme.provider.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping()
 public class ProviderController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ProviderService providerService;
 
     @GetMapping("")
     public ModelAndView showLoginForm() {
@@ -16,9 +29,18 @@ public class ProviderController {
     }
 
     @GetMapping("/cadastro")
-    public ModelAndView showRegistrationForm() {
+    public ModelAndView showRegistrationForm(UserDTO userDTO) {
         ModelAndView mv = new ModelAndView("registration");
         return mv;
+    }
+
+    @PostMapping("/adduser")
+    public String addUser(UserDTO userDTO, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            return "registration";
+        }
+        userService.insert(userDTO);
+        return "redirect:/fornecedores";
     }
 
     @GetMapping("/fornecedores")
